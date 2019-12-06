9001891910fd473466dba16eeccce28489ab57f8
czhowl <czhhzc94@gmail.com>
2019-12-06 00:25:22 -0500

    Update MouseApp.cpp

--- Mouse/src/MouseApp.cpp
+++ Mouse/src/MouseApp.cpp
@@ -2,6 +2,8 @@
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
 
+#include "cinder/Serial.h"
+
 using namespace ci;
 using namespace ci::app;
 using namespace std;
@@ -12,10 +14,25 @@ class MouseApp : public App {
 	void mouseDown( MouseEvent event ) override;
 	void update() override;
 	void draw() override;
+
+	SerialRef                   mSerial;
+	char						mBytes[4];
 };
 
 void MouseApp::setup()
 {
+	// -------------------------------- Serial Communication
+	for (const auto& dev : Serial::getDevices())
+		console() << "Device: " << dev.getName() << endl;
+
+	//try {
+	//	Serial::Device dev = Serial::findDeviceByNameContains("COM5");
+	//	mSerial = Serial::create(dev, 250000);
+	//}
+	//catch (SerialExc & exc) {
+	//	console() << "coult not initialize the serial device " << endl;
+	//	//exit(-1);
+	//}
 }
 
 void MouseApp::mouseDown( MouseEvent event )
@@ -24,6 +41,14 @@ void MouseApp::mouseDown( MouseEvent event )
 
 void MouseApp::update()
 {
+	//if (mSerial->getNumBytesAvailable() > 0) {
+	//	mSerial->readBytes(mBytes, 4);
+	//	int pos1 = mBytes[1] << 8 + mBytes[0];
+	//	int pos2 = mBytes[3] << 8 + mBytes[2];
+	//	console() << pos1 << "   " << pos2 << endl;
+	//}
+	for (const auto& dev : Serial::getDevices())
+		console() << "Device: " << dev.getName() << endl;
 }
 
 void MouseApp::draw()
